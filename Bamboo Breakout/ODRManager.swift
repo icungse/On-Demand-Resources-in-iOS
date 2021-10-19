cbdf71e1ca6d2a1824f70f7eb7096f66fe7b6144
saiful <saiful@mamiteam.com>
2021-10-19 21:01:26 +0700

    Using NSBundleResourceRequest

---
+++ Bamboo Breakout/ODRManager.swift
@@ -0,0 +1,33 @@
+//
+//  ODRManager.swift
+//  Bamboo Breakout
+//
+//  Created by icung on 19/10/21.
+//  Copyright © 2021 Razeware LLC. All rights reserved.
+//
+
+import Foundation
+
+class ODRManager {
+  
+  // Mark: - Properties
+  static let shared = ODRManager()
+  var currentRequest: NSBundleResourceRequest?
+  
+  func requestSceneWith(tag: String, onSuccess: @escaping () -> Void, onFailure: @escaping (NSError) -> Void) {
+    currentRequest = NSBundleResourceRequest(tags: [tag])
+    
+    guard let requset = currentRequest else {
+      return
+    }
+    
+    requset.beginAccessingResources { (error: Error?) in
+      if let error = error {
+        onFailure(error as NSError)
+        return
+      }
+      
+      onSuccess()
+    }
+  }
+}
